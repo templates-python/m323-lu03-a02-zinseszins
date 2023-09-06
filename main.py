2062095aaf78c6008e2b1de3b0211125e5640c9b
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-06 09:53:30 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-def calculate():
+def compound_interest(principal, rate, time):
     """
-    calculates the total
-    :return: None
+    Berechnet das Endguthaben einer Investition unter Berücksichtigung des Zinseszinses.
+
+    Args:
+    principal (float): Der Anfangsbetrag der Investition.
+    rate (float): Der jährliche Zinssatz als Dezimalzahl (z.B. 0.05 für 5%).
+    time (int): Die Laufzeit der Investition in Jahren.
+
+    Returns:
+    float: Das Endguthaben der Investition.
     """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+    # TODO: Implementieren Sie die Funktion rekursiv
+    pass
 
 
 if __name__ == '__main__':
-    calculate()
+    compound_interest(1000, 0.05, 10)
--- main_test.py
+++ main_test.py
@@ -1,6 +1,38 @@
 import main
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+
+def test_compound_interest():
+    # Test mit einem Anfangsbetrag von 1000, 5% Zinssatz und einer Laufzeit von 5 Jahren
+    assert round(main.compound_interest(1000, 0.05, 5), 2) == 1276.28
+
+    # Test mit einem Anfangsbetrag von 2000, 3% Zinssatz und einer Laufzeit von 10 Jahren
+    assert round(main.compound_interest(2000, 0.03, 10), 2) == 2685.68
+
+    # Test mit einem Anfangsbetrag von 5000, 7% Zinssatz und einer Laufzeit von 0 Jahren
+    assert round(main.compound_interest(5000, 0.07, 0), 2) == 5000.00
+
+    # Test mit einem Anfangsbetrag von 0, 5% Zinssatz und einer Laufzeit von 5 Jahren
+    assert round(main.compound_interest(0, 0.05, 5), 2) == 0.00
+
+    # Test mit einem Anfangsbetrag von 1000, 0% Zinssatz und einer Laufzeit von 5 Jahren
+    assert round(main.compound_interest(1000, 0, 5), 2) == 1000.00
+
+
+def test_compound_interest_edge_cases():
+    # Test mit einem negativen Anfangsbetrag
+    assert round(main.compound_interest(-1000, 0.05, 5), 2) == -1276.28
+
+    # Test mit einem negativen Zinssatz (z.B. bei einer Strafgebühr)
+    assert round(main.compound_interest(1000, -0.05, 5), 2) == 770.45
+
+    # Test mit negativer Laufzeit (sollte in der Realität nicht vorkommen, aber gut für die Funktion zu handhaben)
+    assert main.compound_interest(1000, 0.05, -5) == 1000  # In diesem Fall könnte man auch eine Exception werfen
+
+    # Test mit einem Zinssatz von mehr als 100% (also 1.0)
+    assert round(main.compound_interest(1000, 2, 2), 2) == 9000.00
+
+    # Test mit einer sehr hohen Laufzeit
+    assert round(main.compound_interest(1000, 0.01, 1000), 2) == 2.09e+13
+
+    # Test mit einem sehr niedrigen Zinssatz
+    assert round(main.compound_interest(1000, 0.000001, 1000), 2) == 1001.00
